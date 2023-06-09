bd1235e59a0da59efb279dda56151ce78183ddf0
neviko <nevosayag@gmail.com>
2023-06-09 23:05:39 +0300

    postgres in docker compose

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# node-ts-express-eslint-boilerplate
+# Roundforest home assignment - Backend
 
-This boilerplate was created by Nevo Sayag
+## run instructions
+
+    1 - Clone the repo
+    2 - At the root level run "npm i"
+    3 - For running the postgres DB on docker-compose run "docker-compose up"
+    4 - After the DB instance will be created we'll run the migration and seed files
+        4.1 - cd ./db/
+        4.2 - npm run migration
+
+    5 - After creating the DB, we'll start the server by "npm run start:dev"
+
+    6 - Extra - run the jest tests "npm run test"
--- db/knexfile.ts
+++ db/knexfile.ts
@@ -1,15 +1,26 @@
 // Update with your config settings.
-
+// import * as dotenv from "dotenv";
+// dotenv.config({ path: "../.env" });
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+//TODO:fix dotenv to work with knex
 export default {
   development: {
     client: "postgresql",
     connection: {
-      database: "roundforest",
-      user: "postgres",
-      password: "437440",
+      database: "roundforest", // process.env.POSTGRES_DB,
+      user: "postgres", // process.env.POSTGRES_USER,
+      password: "postgres", //process.env.POSTGRES_PASSWORD,
+      host: "localhost", // process.env.POSTGRES_HOST,
+      port: 7000,
+    },
+    migrations: {
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
     },
   },
 
